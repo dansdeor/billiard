62f62e797a98d64da1180985e54b306c1e516eb4
OritM <orit.m@campus.technion.ac.il>
2022-08-22 20:05:05 +0300

    One hole is completed, the second hole has a bug in the bdf file

--- objects_mux.sv
+++ objects_mux.sv
@@ -1,9 +1,25 @@
 module objects_mux (
 						input logic	clk,
-						input logic	resetN,	
-						// board layout
+						input logic	resetN,
+					
+						//holes layout
+						input logic drawingRequestHole_1,
+						input logic	[7:0] RGBHole_1, 	
+						input logic drawingRequestHole_2,
+						input logic	[7:0] RGBHole_2,
+						input logic drawingRequestHole_3,
+						input logic	[7:0] RGBHole_3,
+						input logic drawingRequestHole_4,
+						input logic	[7:0] RGBHole_4,
+						input logic drawingRequestHole_5,
+						input logic	[7:0] RGBHole_5,
+						input logic drawingRequestHole_6,
+						input logic	[7:0] RGBHole_6, 
+						
+						// borders layout
 						input logic drawingRequestBorders,
 						input logic	[7:0] RGBBorders,
+						
 						// board layout
 						input logic drawingRequestBoard,
 						input logic	[7:0] RGBBoard, 
@@ -18,7 +34,25 @@ begin
 	end
 	
 	else begin
-		if (drawingRequestBorders == 1'b1) begin
+		if (drawingRequestHole_1 == 1'b1) begin
+			RGBOut <= RGBHole_1;
+		end
+		else if (drawingRequestHole_2 == 1'b1) begin
+			RGBOut <= RGBHole_2;
+		end
+		else if (drawingRequestHole_3 == 1'b1) begin
+			RGBOut <= RGBHole_3;
+		end
+		else if (drawingRequestHole_4 == 1'b1) begin
+			RGBOut <= RGBHole_4;
+		end
+		else if (drawingRequestHole_5 == 1'b1) begin
+			RGBOut <= RGBHole_5;
+		end
+		else if (drawingRequestHole_6 == 1'b1) begin
+			RGBOut <= RGBHole_6;
+		end
+		else if (drawingRequestBorders == 1'b1) begin
 			RGBOut <= RGBBorders;
 		end
 		else if (drawingRequestBoard == 1'b1) begin
