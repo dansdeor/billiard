9500d34cb9ce2af3797bf3b7327ff6738c6fd3bd
Dan Sdeor <dansdeor@campus.technion.ac.il>
2022-08-26 20:07:52 +0300

    Now we have two balls in the game

--- RTL/HIT_CONTROLLER/hit_controller.sv
+++ RTL/HIT_CONTROLLER/hit_controller.sv
@@ -1,40 +1,40 @@
 module hit_controller (
-					input logic clk,
-					input logic resetN,
-					
-					input logic whiteBallDR,
-					input logic redBallDR,
-					input logic bordersDR,
-				
-					input logic holeDR1,
-					input logic holeDR2,
-					input logic holeDR3,
-					input logic holeDR4,
-					input logic holeDR5,
-					input logic holeDR6,
+	input logic clk,
+	input logic resetN,
 
-					//ball position and velocity (velocities are most important for ball and border collision detection)
-					input logic signed [10:0] whiteBallTopLeftPosX,
-					input logic signed [10:0] whiteballTopLeftPosY,
-					input logic signed [10:0] whiteBallVelX,
-					input logic signed [10:0] whiteBallVelY,
-					
-					input logic signed [10:0] redBallTopLeftPosX,
-					input logic signed [10:0] redballTopLeftPosY,
-					input logic signed [10:0] redBallVelX,
-					input logic signed [10:0] redBallVelY,
-					
-					output logic signed [10:0] whiteBallVelXOut,
-					output logic signed [10:0] whiteBallVelYOut,
-					output logic whiteBallCollisionOccurred,
-					output logic whiteBallHoleHit,
-					output logic [2:0] whiteBallHoleNum,
-					
-					output logic signed [10:0] redBallVelXOut,
-					output logic signed [10:0] redBallVelYOut,
-					output logic redBallCollisionOccurred,
-					output logic redBallHoleHit,
-					output logic [2:0] redBallHoleNum
+	input logic whiteBallDR,
+	input logic redBallDR,
+	input logic bordersDR,
+
+	input logic holeDR1,
+	input logic holeDR2,
+	input logic holeDR3,
+	input logic holeDR4,
+	input logic holeDR5,
+	input logic holeDR6,
+
+	//ball position and velocity (velocities are most important for ball and border collision detection)
+	input logic signed [10:0] whiteBallTopLeftPosX,
+	input logic signed [10:0] whiteballTopLeftPosY,
+	input logic signed [10:0] whiteBallVelX,
+	input logic signed [10:0] whiteBallVelY,
+
+	input logic signed [10:0] redBallTopLeftPosX,
+	input logic signed [10:0] redballTopLeftPosY,
+	input logic signed [10:0] redBallVelX,
+	input logic signed [10:0] redBallVelY,
+
+	output logic signed [10:0] whiteBallVelXOut,
+	output logic signed [10:0] whiteBallVelYOut,
+	output logic whiteBallCollisionOccurred,
+	output logic whiteBallHoleHit,
+	output logic [2:0] whiteBallHoleNum,
+
+	output logic signed [10:0] redBallVelXOut,
+	output logic signed [10:0] redBallVelYOut,
+	output logic redBallCollisionOccurred,
+	output logic redBallHoleHit,
+	output logic [2:0] redBallHoleNum
 );
 
 parameter int TOP_OFFSET = 0, DOWN_OFFSET = 479, LEFT_OFFSET = 0, RIGHT_OFFSET = 639;
@@ -61,7 +61,21 @@ border_collision #(TOP_OFFSET, DOWN_OFFSET, LEFT_OFFSET, RIGHT_OFFSET) white_bal
 	.collisionOccurred(borderWhiteBallCol)
 );
 
-//TODO: add mux between cols
+border_collision #(TOP_OFFSET, DOWN_OFFSET, LEFT_OFFSET, RIGHT_OFFSET) red_ball_border_col(
+	.clk(clk),
+	.resetN(resetN),
+	.ballDR(redBallDR),
+	.bordersDR(bordersDR),
+	.ballTopLeftPosX(redBallTopLeftPosX),
+	.ballTopLeftPosY(redballTopLeftPosY),
+	.ballVelX(redBallVelX),
+	.ballVelY(redBallVelY),
+	.ballVelXOut(borderRedBallVelX),
+	.ballVelYOut(borderRedBallVelY),
+	.collisionOccurred(borderRedBallCol)
+);
+
+//TODO: add mux between cols of border and ball
 //for now
 always_comb begin
 	if(borderWhiteBallCol) begin
@@ -74,6 +88,19 @@ always_comb begin
 	end
 end
 
+always_comb begin
+	if(borderWhiteBallCol) begin
+		redBallVelXOut = borderRedBallVelX;
+		redBallVelYOut = borderRedBallVelY;
+	end
+	else begin
+		redBallVelXOut = 11'b0;
+		redBallVelYOut = 11'b0;
+	end
+end
+
+// TODO: add OR with ball collision
 assign whiteBallCollisionOccurred = borderWhiteBallCol;
+assign redBallCollisionOccurred = borderRedBallCol;
 
 endmodule
